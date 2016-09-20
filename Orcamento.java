411e1d36759bfd91afeabe836cb29d0db3631c25
Iann Lucas <iann.lucas16@gmail.com>
2016-09-20 11:06:59 -0300

    prova commit

---
+++ Orcamento.java
@@ -0,0 +1,30 @@
+package br.ifpi.edu.aula.pc.boasPraticas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Orcamento {
+
+	private double valor;
+	private List<Item> itens;
+	
+	public Orcamento(double valor) {
+		this.valor = valor;
+		this.itens = new ArrayList<>();
+	}
+
+	public double getValor() {
+		return valor;
+	}
+	
+	public List<Item> getItens(){
+		return itens;
+	}
+	
+	public void adicionaItem(Item item){
+		itens.add(item);
+	}
+	
+
+	
+}
